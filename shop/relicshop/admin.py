beb494e506be9cd3800d431ea7986babb0f7cd40
RelicR <LKJ-J3t-2nA-b44>
2024-07-05 01:19:36 +0400

    Registered entity models

--- shop/relicshop/admin.py
+++ shop/relicshop/admin.py
@@ -4,6 +4,18 @@ from . import models
 
 
 @admin.register(models.CityModel)
-class PostFilesAdmin(admin.ModelAdmin):
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name']
+
+
+@admin.register(models.StreetModel)
+class StreetAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name']
+
+
+@admin.register(models.ShopModel)
+class ShopAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ['name']
